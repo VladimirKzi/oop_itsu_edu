270c45c93a184d37667833e4238c1e1439be6769
Vladimir <vladimir.kzier@gmail.com>
2026-03-08 22:03:49 +0200

    Add files via upload

---
+++ ConsoleApplicationSem2Lab4/ConsoleApplicationSem2Lab4/ConsoleApplicationSem2Lab4.cpp
@@ -0,0 +1,42 @@
+﻿// ConsoleApplicationSem2Lab4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+template <class T>
+void procVec(vector<T>& v)
+{
+	for (int i = 0; i < v.size(); i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	cout << "Відсортовані вектори:\n";
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	int n;
+
+	cout << "Введіть розмір: ";
+	cin >> n;
+
+	vector<int> v(n);
+
+	cout << "Введіть елементи:\n";
+	procVec(v);
+
+	return 0;
+}
